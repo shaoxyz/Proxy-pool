708fba1adbb0d6253258aa9ba719f2c957fce16e
Shwb <shwb95@163.com>
2017-08-18 13:19:41 +0800

    http(s) 代理

--- pool/gethttps.py
+++ pool/gethttps.py
@@ -16,7 +16,7 @@ from utils import errorCatch
 
 class GetFreeProxy(object):
     """
-    proxy getter
+    http(s) proxy getter
     """
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
